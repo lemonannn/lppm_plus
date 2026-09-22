2a88a56d70b3f8628d869f4489d86579d02f3a19
lemonannn <kruger.jack17@gmail.com>
2026-09-22 16:23:49 -0400

    basic data gotten

--- get_data.py
+++ get_data.py
@@ -1,4 +1,14 @@
-import pybaseball
+from fungo import fangraphs
+import pandas as pd
 
-# Pull individual pitching stats for a specific season
-pitching_data = pybaseball.pitching_stats(2023)
+
+def create_dataset():
+    pitchers = fangraphs.get_leaders(stats="pit", start_season=2015, end_season=2025, qual=100, ind=1)
+    pitchers_df = pd.DataFrame(pitchers)
+    df = pitchers_df[['PlayerName', 'FIP-', 'Season', 'piwFA', 'piwFC', 'piwFS', 'piwSI', 'piwCH', 'piwSL', 'piwCU']]
+    df.to_csv('arsenal_data.csv')
+    
+    return df
+
+
+arsenal_df = create_dataset()
